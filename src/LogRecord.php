bf958be788e4b7d70fe180c2b9fec153e11e395a
Chris Bautista <chris.bautista@coreproc.com>
2024-03-25 08:47:37 +0800

    Remove datetime from custom logs

--- src/LogRecord.php
+++ src/LogRecord.php
@@ -7,6 +7,11 @@ class LogRecord extends \Monolog\LogRecord
     public function toArray(): array
     {
         $array = parent::toArray();
+
+        // Remove datetime
+        unset($array['datetime']);
+
+        // Add origin
         return array_merge(['origin' => 'app.' . config('custom-logging.container_role')], $array);
     }
 }
